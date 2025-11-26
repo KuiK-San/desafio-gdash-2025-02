08fc667524e93e462af8471f42198753232011d6
Guilherme Casagrande <thegui4000@gmail.com>
2025-11-26 14:28:04 -0300

    test<envTest>: set envioroment tests

---
+++ meteorologyCollector/src/tests/test_collector.py
@@ -0,0 +1,25 @@
+import pytest
+import os
+from unittest.mock import patch
+from app.collector.main import ClimateClient
+
+@pytest.fixture
+def env_mock(monkeypatch):
+    monkeypatch.setenv("CLIMATE_API_KEY", "API_KEY.CLIMATE")
+    monkeypatch.setenv("CLIMATE_API_URL", "https://climate-url.com")
+    
+def test_init_loads_env(monkeypatch):
+    monkeypatch.setenv("CLIMATE_API_KEY", "ABC")
+    monkeypatch.setenv("CLIMATE_API_URL", "https://newapi-url.com")
+    
+    client = ClimateClient(lat=1, lon=2)
+    
+    assert client.api_key == "ABC"
+    assert client.api_url == "https://newapi-url.com"
+    
+    
+def test_init_without_env_api_key(monkeypatch):
+    monkeypatch.delenv("CLIMATE_API_KEY", raising=False)
+    monkeypatch.delenv("CLIMATE_API_URL", raising=False)
+    
+    with pytest.raises(ValueError): ClimateClient(lat=1, lon=2)
