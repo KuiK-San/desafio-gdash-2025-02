0966cf1e576d2fa786b34bf9bd115312f7dc2556
Guilherme Casagrande <thegui4000@gmail.com>
2025-11-26 16:17:39 -0300

    feat<buildURL>: criando metodo de build da URL

--- meteorologyCollector/src/app/collector/main.py
+++ meteorologyCollector/src/app/collector/main.py
@@ -1,12 +1,15 @@
 import os
+from urllib.parse import urlencode
 from dotenv import load_dotenv
 
 load_dotenv()
 
 class ClimateClient:
-    def __init__(self, lat: float, lon: float):
+    def __init__(self, lat: float, lon: float, units: str = "metric", lang: str = "pt_br"):
         self.lat = lat
         self.lon = lon
+        self.lang = lang
+        self.units = units
         
         self.api_key = os.getenv("CLIMATE_API_KEY")
         self.api_url = os.getenv("CLIMATE_API_URL")
@@ -16,3 +19,17 @@ class ClimateClient:
         
         if not self.api_url: 
             raise ValueError("CLIMATE_API_URL is not defined")
+        
+        if self.units not in ["standard", "metric", "imperial"]:
+            raise ValueError("Unit must be metric, imperial or standard")
+        
+    def _build_url(self):
+        params = {
+            'lat': self.lat,
+            'lon': self.lon,
+            'appid': self.api_key,
+            'units': self.units,
+            'lang': self.lang
+        }
+        
+        return f"{self.api_url}?{urlencode(params)}"
